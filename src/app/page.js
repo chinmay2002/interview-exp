3145863a8b438e579b87901d9ed64c4d431a36b6
Sandeep Behl <sbehl20-csbs@bvucoep.edu.in>
2023-10-05 21:01:39 +0530

    Theme Setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,29 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+"use client";
+
+import Navbar from "@/components/Navbar";
+import React, { useState, useEffect } from "react";
+import { Container } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createCustomTheme } from "../utilities/theme";
 
 const page = () => {
-  return (
-  <>
-  Hello World
-  </>
-  )
-}
+  const [theme, setTheme] = useState({});
 
-export default page
+  useEffect(() => {
+    setTheme(createCustomTheme());
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Navbar />
+        </ThemeProvider>
+      </Container>
+    </>
+  );
+};
+
+export default page;
