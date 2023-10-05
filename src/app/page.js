436e53d33b76542d87b52e60007e1728f9c03c07
Sandeep Behl <sbehl20-csbs@bvucoep.edu.in>
2023-10-05 21:14:00 +0530

    "Theme Setup"

--- src/app/page.js
+++ src/app/page.js
@@ -6,25 +6,9 @@ import { Container } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createCustomTheme } from "../utilities/theme";
-import Login from "@/app/signUp/page";
 
 const page = () => {
-  const [theme, setTheme] = useState({});
-
-  useEffect(() => {
-    setTheme(createCustomTheme());
-  }, []);
-
-  return (
-    <>
-      <Container>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Navbar />
-        </ThemeProvider>
-      </Container>
-    </>
-  );
+  return <>Hello World</>;
 };
 
 export default page;
